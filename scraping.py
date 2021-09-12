a63d0d30d64240a8ab7690dba57e660323823e86
seancary62 <sean.cary62@gmail.com>
2021-09-11 21:45:36 -0400

    format changes to html file, added images folder for image used in jumbotron background

--- scraping.py
+++ scraping.py
@@ -20,7 +20,7 @@ def scrape_all():
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
         "last_modified": dt.datetime.now(),
-        "hemispheres" : {hemispheres(browser)}
+        "hemispheres" : hemispheres(browser)
     }
 
     # Stop webdriver and return data
